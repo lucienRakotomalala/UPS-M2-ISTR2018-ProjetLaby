cc84b94dce51310b475b3b1cf0be9527d4d7a769
lucienRakotomalala <32492420+lucienRakotomalala@users.noreply.github.com>
2017-10-25 20:45:37 +0200

    Essai objet Matlab

---
+++ Objet.m
@@ -0,0 +1,14 @@
+classdef Objet < Laby
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+        
+    properties
+        positionX;
+        positionY;
+    end
+    
+    methods 
+    end
+    
+end
+
