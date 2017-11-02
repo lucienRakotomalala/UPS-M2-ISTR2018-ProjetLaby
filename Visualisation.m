7196cd44ce0d8e3da85d3067e4090da5e9cbe49a
johanne <johannebakalara@gmail.com>
2017-11-02 11:42:23 +0100

    Vue_mur NO Fonctionnel - V1
    TO DO : Correction bug

--- Visualisation.m
+++ Visualisation.m
@@ -15,33 +15,32 @@ classdef Visualisation
         function obj=Visualisation(handles,pacman,ghost,murs)
             obj.pacman = pacman;
             obj.ghost = ghost;
-            obj.murs=murs;
+            obj.murs =murs;
             
         end
         
         %fonction qui affiche les murs que pacman voit
         function  vue_pacman(handles,obj)
             
-            
-            if(deplacementHautPossible(handles, obj.pacman, obj.murs) == 0) %il y a un mur
+            if(deplacementHautPossible(obj.pacman, obj.murs) == 0) %il y a un mur
                 set(handles.PacmanUp,'BackgroundColor','b');
             else
                 set(handles.PacmanUp,'BackgroundColor',[0.8 0.8 0.8]);
             end
             
-            if(deplacementBasPossible(handles, obj.pacman, obj.murs)==0)
+            if(deplacementBasPossible(obj.pacman, obj.murs)==0)
                 set(handles.PacmanDown,'BackgroundColor','b');
             else
                 set(handles.PacmanDown,'BackgroundColor',[0.8 0.8 0.8]);
             end
             
-            if(deplacementGauchePossible(handles, obj.pacman, obj.murs)==0)
+            if(deplacementGauchePossible(obj.pacman, obj.murs)==0)
                 set(handles.PacmanLeft,'BackgroundColor','b');
             else
                 set(handles.PacmanLeft,'BackgroundColor',[0.8 0.8 0.8]);
             end
             
-            if(deplacementDroitePossible(handles, obj.pacman, obj.murs)==0)
+            if(deplacementDroitePossible(obj.pacman, obj.murs)==0)
                 set(handles.PacmanRight,'BackgroundColor','b');
             else
                 set(handles.PacmanRight,'BackgroundColor',[0.8 0.8 0.8]);
