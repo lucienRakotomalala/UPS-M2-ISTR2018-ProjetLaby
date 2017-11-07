dd02c7685ef91e0dd689765d64b31102cebecad3
johanne <johannebakalara@gmail.com>
2017-11-07 17:19:38 +0100

    Premier Exemple d'implémentation de modelLaby
    Murs Fonctionnels. TODO : Objets

--- ModelLaby.m
+++ ModelLaby.m
@@ -7,20 +7,29 @@ classdef ModelLaby < ModelSED
     end
     
     methods
-        
-        
+        function obj = ModelLaby(state)
+            obj.presentState = state
+        end
         
         % --- Evolution of the labyrinth 
-        function nextState = f(presentState,in)
-            ...
+        function nextState = f(obj, in)
+            if(in(2) == 1) % Walls Vertical
+                nextState.walls = obj.presentState.walls.moveVerticalWalls();
+            end
+            if(in(3) == 1) % Walls Horizontal
+                nextState.walls = obj.presentState.walls.moveHorizontalWalls();
+            end
+%             if(in(4) == 1) % Pacman left
+%                 nextState.pacman = obj.presentState.pacman.goLeft();
+%             end
         end
         % --- Memory 
-        function presentState = m(nextState,init)
-            ...
+        function m(obj,nextState, init)
+            obj.presentState = nextState;
         end
         % -- Generation of the output 
-        function  out = g(presentState,in)
-            ...
+        function  out = g(obj, in)
+            out = obj.presentState.walls;
         end
     end
     
