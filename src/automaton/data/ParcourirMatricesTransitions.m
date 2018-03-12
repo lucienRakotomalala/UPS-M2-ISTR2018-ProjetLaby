52e20c32d560dbed79fc9a773ac4acc202d3e885
Lucien <lulu.rakoto@gmail.com>
2018-03-12 18:52:05 +0100

    Validation de ParcourirMatricesTransitions
    Sur un petit procédé, tout est OK. Modification sur le corps de
    l'algorithme de Bellman.

--- src/automaton/data/ParcourirMatricesTransitions.m
+++ src/automaton/data/ParcourirMatricesTransitions.m
@@ -3,23 +3,23 @@ function [Buff]=ParcourirMatricesTransitions(MatricesTransition, Poids)
             
             nb_states=length(MatricesTransition);
             Buff=zeros(3,nb_states);
-            Buff(1,2:end)=+inf;
+            Buff(1,2:end)=+inf
             Etat_courant=1;
-            %%
-           while Etat_courant<=nb_states
+            
+           while Etat_courant <= nb_states
                %%
               [~,c]= find(MatricesTransition(Etat_courant,:)~=0)
               for k=1:length(c)
-                dist=Buff(1,Etat_courant)+Poids(1,c(k))
+                dist = min(Buff(1,c(k)), Buff(1,Etat_courant) + Poids(1,c(k)))
                 
-                if(dist<Buff(1,c(k)))
-                    Buff(2,c(k))=MatricesTransition(Etat_courant,c(k));
-                    Buff(1,c(k))=dist;
-                    Buff(3,c(k))=Etat_courant;
-                end
+                %    if(dist<Buff(1,c(k)))  J'ai pompé sur Wiki ...
+                        Buff(2,c(k))=MatricesTransition(Etat_courant,c(k));
+                        Buff(1,c(k))=dist;
+                        Buff(3,c(k))=Etat_courant;
+                 %   end
                 
               end
-              %%
+              
                 Etat_courant=Etat_courant+1
             
             end
