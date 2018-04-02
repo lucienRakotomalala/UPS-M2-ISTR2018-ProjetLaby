f5af835a60d1dd39e8e8b5cd82d7a08ff5ba5022
David <david.tocaven@gmail.com>
2018-04-02 13:08:18 +0100

    Exclusion fichiers non concernés + réorga fichiers + comment lanceur

--- src/laby1player/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# script qui compile le doxy, make le pdf et l'ouvre
-doxygen Doxyfile
-cd latex/
-make all
-#evince refman.pdf &
-cd ..
--- src/laby2player/doc/html/menudata.js
+++ src/laby2player/doc/html/menudata.js
@@ -47,23 +47,5 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
-{text:"All",url:"globals.html",children:[
-{text:"a",url:"globals.html#index_a"},
-{text:"c",url:"globals.html#index_c"},
-{text:"f",url:"globals.html#index_f"},
-{text:"g",url:"globals.html#index_g"},
-{text:"i",url:"globals.html#index_i"},
-{text:"r",url:"globals.html#index_r"},
-{text:"s",url:"globals.html#index_s"},
-{text:"u",url:"globals.html#index_u"},
-{text:"w",url:"globals.html#index_w"}]},
-{text:"Functions",url:"globals_func.html",children:[
-{text:"a",url:"globals_func.html#index_a"},
-{text:"c",url:"globals_func.html#index_c"},
-{text:"f",url:"globals_func.html#index_f"},
-{text:"g",url:"globals_func.html#index_g"},
-{text:"i",url:"globals_func.html#index_i"},
-{text:"r",url:"globals_func.html#index_r"},
-{text:"s",url:"globals_func.html#index_s"},
-{text:"u",url:"globals_func.html#index_u"},
-{text:"w",url:"globals_func.html#index_w"}]}]}]}]}
+{text:"All",url:"globals.html"},
+{text:"Functions",url:"globals_func.html"}]}]}]}
